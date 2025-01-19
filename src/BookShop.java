20bbe8e859083fea5f5e136c6f6137c62f3112f8
saima2208 <saimasonya22@gmail.com>
2025-01-19 17:42:16 +0600

    FILE UPDATE

--- src/BookShop.java
+++ src/BookShop.java
@@ -4,21 +4,27 @@ import java.time.LocalDate;
 public class BookShop {
 
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/ORCLPDB";
+    private static final String USER = "orclpdbuser";
+    private static final String PASSWORD = "isdb62";
+/*
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/ORCLPDB";
     private static final String USER = "postgres";
     private static final String PASSWORD = "isdb62";
 
+ */
+
     public void insertDummtData(String[][] books) {
-        String insertQuery = "INSERT INTO BOOKSHOP (name, author,price,publishDate) VALUES (?, ?, ?, ?)";
+        String insertQuery = "INSERT INTO BOOKSHOP (NAME, AUTHOR_NAME, PRICE, GENRE,PUBLISHED_DATE) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
              PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
             for (String[] book : books) {
                 preparedStatement.setString(1, book[0]);
                 preparedStatement.setString(2, book[1]);
-               // preparedStatement.setString(3, book[2]);
-                preparedStatement.setDouble(3,Double.parseDouble(book[2]));
-                preparedStatement.setDate(4, Date.valueOf(book[3]));
+               preparedStatement.setInt(3, Integer.parseInt(book[2]));
+                preparedStatement.setString(4,book[3]);
+                preparedStatement.setDate(5, Date.valueOf(book[4]));
 
                 preparedStatement.addBatch();
 
--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,10 @@ public class Main {
 
  */
         String[][] books = {
-                {"MacBeth", "Shakespeare",  "500", "2000-06-6"},
-                {"Merchant of Vanice", "Shakespeare",  "400", "1999-06-6"},
-                {"King Lear", "Shakespeare","300", "2001-06-6"},
-                {"Doctor Faustus", "Christofer Marlow", "700", "2002-06-6"}
+                {"MacBeth", "Shakespeare",  "500", "DRAMA","2000-06-6"},
+                {"Merchant of Vanice", "Shakespeare", "400","DRAMA", "1999-06-6"},
+                {"King Lear", "Shakespeare","300","DRAMA", "2001-06-6"},
+                {"Doctor Faustus", "Christofer Marlow", "700","DRAMA", "2002-06-6"}
 
         };
         BookShop bookShop = new BookShop();
